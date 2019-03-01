e6a7d8b291ffc578b20fc43de06c7485e3fb71da
Abder-Rahim <contact@developpeur-csharp.com>
2019-03-01 21:16:54 +0100

    Version 1.0

---
+++ FileReaderUtility/FileReaderUtilityForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FileReaderUtility
+{
+    partial class FileReaderUtilityForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileContent = new System.Windows.Forms.TextBox();
+            this.ofdOpen = new System.Windows.Forms.OpenFileDialog();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFileContent
+            // 
+            this.txtFileContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFileContent.Location = new System.Drawing.Point(3, 55);
+            this.txtFileContent.Multiline = true;
+            this.txtFileContent.Name = "txtFileContent";
+            this.txtFileContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtFileContent.Size = new System.Drawing.Size(794, 393);
+            this.txtFileContent.TabIndex = 0;
+            // 
+            // ofdOpen
+            // 
+            this.ofdOpen.FileName = "openFileDialog1";
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSelectFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectFile.Location = new System.Drawing.Point(0, 0);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(800, 36);
+            this.btnSelectFile.TabIndex = 1;
+            this.btnSelectFile.Text = "Select your file";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(12, 39);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(0, 13);
+            this.lblFilePath.TabIndex = 2;
+            // 
+            // FileReaderUtilityForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.txtFileContent);
+            this.Name = "FileReaderUtilityForm";
+            this.Text = "FileReaderUtility";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileContent;
+        private System.Windows.Forms.OpenFileDialog ofdOpen;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Label lblFilePath;
+    }
+}
+
---
+++ FileReaderUtility/FileReaderUtilityForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FileReaderUtility
+{
+    public partial class FileReaderUtilityForm : Form
+    {
+        public FileReaderUtilityForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    lblFilePath.Text = filePath;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                        txtFileContent.Text = fileContent;
+                    }
+                }
+            }
+        }
+    }
+}
